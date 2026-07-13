63bfed85b300d05ec2b1ed751cb575021935ba20
javiercondecortes <jccfmys@gmail.com>
2026-07-13 20:47:32 +0200

    creación de proyecto

---
+++ src/app/candeonato/components/form-inscription/form-inscription.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-inscription',
+  imports: [],
+  templateUrl: './form-inscription.html',
+  styleUrl: './form-inscription.css',
+})
+export class FormInscription {}
---
+++ src/app/candeonato/components/requirements/requirements.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-requirements',
+  imports: [],
+  templateUrl: './requirements.html',
+  styleUrl: './requirements.css',
+})
+export class Requirements {}
